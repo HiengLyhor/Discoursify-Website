6c8c58b618617f3900840e3d62acd1e6ceba88ac
Lyhor Hieng <hieng.lyhoir6764@gmail.com>
2024-02-06 17:51:59 +0700

    Commit first deploy project into git

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Discoursify.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
